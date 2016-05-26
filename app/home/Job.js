8baa4265317c1287810275bfa80eb90199fad7a8
wjdimba <15058022590@163.com>
2016-05-26 22:38:55 +0800

    微调列表的样式

--- app/home/Job.js
+++ app/home/Job.js
@@ -37,9 +37,9 @@ var Job = React.createClass({
               <Text style={styles.roundLayoutText}>兼职</Text>
             </View>
             <View style={{flexDirection:'column'}}>
-              <Text style={{marginLeft:10}}>找聘兼职人员</Text>
-              <Text style={{marginLeft:10}}>余杭 5月1日起（共30天）</Text>
-              <Text style={{marginLeft:10}}>2016-05-25 14:15:57</Text>
+              <Text style={{marginLeft:10,fontSize:14}}>找聘兼职人员</Text>
+              <Text style={{marginLeft:10,fontSize:14}}>余杭 5月1日起（共30天）</Text>
+              <Text style={{marginLeft:10,fontSize:14}}>2016-05-25 14:15:57</Text>
             </View>
           </View>
           }
@@ -54,11 +54,12 @@ var styles = StyleSheet.create({
 
   container:{
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#000000',
   },
 
   roundLayoutText:{
     color : '#ffffff',
+
   },
 
   roundLayout:{
@@ -68,14 +69,15 @@ var styles = StyleSheet.create({
     borderRadius:20,
     alignItems:'center',
     justifyContent: 'center',
-    marginTop:5,
+    marginTop:10,
   },
 
   jobItem:{
     flex: 1,
-    height:100,
+    height:80,
     flexDirection: 'row',
     padding:10,
+    backgroundColor:'#ffffff',
   },
 
   toolbar: {
